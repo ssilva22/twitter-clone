3c99c13b33699bf032f88ed8146765e3ee6fa509
Sebastian <sebastian@SEBASTIAN-01.local>
2021-07-20 10:55:05 -0400

    css

--- my-app/src/Tweetbox.js
+++ my-app/src/Tweetbox.js
@@ -28,7 +28,7 @@ function TweetBox() {
     <div className="tweetBox">
       <form>
         <div className="tweetBox__input">
-          <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+          <Avatar src="https://pbs.twimg.com/profile_images/1365295896502685696/KKPZBWcB_400x400.jpg" />
           <input
             onChange={(e) => setTweetMessage(e.target.value)}
             value={tweetMessage}
